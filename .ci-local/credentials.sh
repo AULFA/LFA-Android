d7adeedd39d964deda8db1e9986ab12e3d02bbc9
Mark Raynsford <code@io7m.com>
2021-01-19 11:21:50 +0000

    Add credential copying

---
+++ .ci-local/credentials.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#------------------------------------------------------------------------
+# A script to copy the necessary credentials into the right places in the
+# build.
+#
+
+#------------------------------------------------------------------------
+# Utility methods
+#
+
+fatal()
+{
+  echo "credentials.sh: fatal: $1" 1>&2
+  exit 1
+}
+
+info()
+{
+  echo "credentials.sh: info: $1" 1>&2
+}
+
+copy()
+{
+  FROM="$1"
+  TO="$2"
+  cp -v "${FROM}" "${TO}" || fatal "could not copy ${FROM} -> ${TO}"
+}
+
+copy .ci/credentials/lfa-keystore.jks lfa-keystore.jks
+copy .ci/credentials/online-app-credentials.json one.lfa.android.app.grande/src/main/assets/account_bundled_credentials.json
+copy .ci/credentials/online-app-credentials.json one.lfa.android.app.laos.online/src/main/assets/account_bundled_credentials.json
+copy .ci/credentials/online-app-credentials.json one.lfa.android.app.laos/src/main/assets/account_bundled_credentials.json
+copy .ci/credentials/online-app-credentials.json one.lfa.android.app.online/src/main/assets/account_bundled_credentials.json
+copy .ci/credentials/lfaAnalytics.xml one.lfa.android.app.grande/src/main/assets/lfaAnalytics.xml
+copy .ci/credentials/lfaAnalytics.xml one.lfa.android.app.laos.online/src/main/assets/lfaAnalytics.xml
+copy .ci/credentials/lfaAnalytics.xml one.lfa.android.app.laos/src/main/assets/lfaAnalytics.xml
+copy .ci/credentials/lfaAnalytics.xml one.lfa.android.app.online/src/main/assets/lfaAnalytics.xml
+copy .ci/credentials/lfaAnalytics.xml one.lfa.android.app.png_offline/src/main/assets/lfaAnalytics.xml
+copy .ci/credentials/lfaAnalytics.xml one.lfa.android.app.timor/src/main/assets/lfaAnalytics.xml
