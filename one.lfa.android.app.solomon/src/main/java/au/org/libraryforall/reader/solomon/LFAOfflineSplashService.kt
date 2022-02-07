eacfc4cf18f0fb8062001571d01dc7504a17a2a8
Andrea Jonus <j.jonus.j@gmail.com>
2022-02-07 13:00:25 +1100

    Add Solomon offline app

---
+++ one.lfa.android.app.solomon/src/main/java/au/org/libraryforall/reader/solomon/LFAOfflineSplashService.kt
@@ -0,0 +1,21 @@
+package au.org.libraryforall.reader.solomon
+
+import org.nypl.simplified.ui.branding.BrandingSplashServiceType
+
+/**
+ * A splash service for the app.
+ */
+
+class LFAOfflineSplashService : BrandingSplashServiceType {
+
+  override val shouldShowLibrarySelectionScreen: Boolean =
+    true
+
+  override fun splashImageResource(): Int {
+    return R.drawable.splash
+  }
+
+  override fun splashImageTitleResource(): Int {
+    return R.drawable.splash
+  }
+}
