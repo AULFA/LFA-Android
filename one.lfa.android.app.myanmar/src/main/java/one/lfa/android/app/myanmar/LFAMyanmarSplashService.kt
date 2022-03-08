58cf776386ad68698404cdafa09d067e8a8131f1
Andrea Jonus <j.jonus.j@gmail.com>
2022-03-08 19:57:08 +1100

    Add Myanmar offline app module

---
+++ one.lfa.android.app.myanmar/src/main/java/one/lfa/android/app/myanmar/LFAMyanmarSplashService.kt
@@ -0,0 +1,21 @@
+package one.lfa.android.app.myanmar
+
+import org.nypl.simplified.ui.branding.BrandingSplashServiceType
+
+/**
+ * A splash service for the app.
+ */
+
+class LFAMyanmarSplashService : BrandingSplashServiceType {
+
+  override val shouldShowLibrarySelectionScreen: Boolean =
+    false
+
+  override fun splashImageResource(): Int {
+    return R.drawable.splash
+  }
+
+  override fun splashImageTitleResource(): Int {
+    return R.drawable.splash
+  }
+}
