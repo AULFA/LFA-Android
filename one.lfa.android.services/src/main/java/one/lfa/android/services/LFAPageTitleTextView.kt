d850ee4a405a3c38baddc3411f9ac76648805cb2
Andrea Jonus <j.jonus.j@gmail.com>
2022-03-11 18:19:36 +1100

    Add doc for the custom TextView

--- one.lfa.android.services/src/main/java/one/lfa/android/services/LFAPageTitleTextView.kt
+++ one.lfa.android.services/src/main/java/one/lfa/android/services/LFAPageTitleTextView.kt
@@ -4,6 +4,16 @@ import android.content.Context
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatTextView
 
+/**
+ * A simple extension of AppCompatTextView that handles cases where we're getting corrupted page titles from NYPL-Simplified/Simplified-Android-Core.
+ * This happens (rarely) when we're handling languages that use non-latin characters. As of 11-03-2022 we have only instance of this happening with LFA's books.
+ *
+ * @constructor
+ * Same as AppCompatTextView
+ *
+ * @param context
+ * @param attrs
+ */
 class LFAPageTitleTextView(context: Context, attrs: AttributeSet?) : AppCompatTextView(context, attrs) {
 
   override fun setText(text: CharSequence, type: BufferType?) {
