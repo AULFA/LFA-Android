79cd4c214c53bc03aa09fd501300401f34b370c3
Andrea Jonus <j.jonus.j@gmail.com>
2022-04-01 15:14:52 +1100

    Add Vietnam online app (#44)

---
+++ one.lfa.android.app.vietnam.online/src/main/java/one/lfa/android/app/vietnam/online/LFAVietnamAccountsFallback.kt
@@ -0,0 +1,36 @@
+package one.lfa.android.app.vietnam.online
+
+import org.joda.time.DateTime
+import org.nypl.simplified.accounts.api.AccountProvider
+import org.nypl.simplified.accounts.api.AccountProviderAuthenticationDescription
+import org.nypl.simplified.accounts.api.AccountProviderFallbackType
+import org.nypl.simplified.accounts.api.AccountProviderType
+import java.net.URI
+
+class LFAVietnamAccountsFallback : AccountProviderFallbackType {
+  override fun get(): AccountProviderType =
+    AccountProvider(
+      addAutomatically = true,
+      annotationsURI = null,
+      authentication = AccountProviderAuthenticationDescription.Anonymous,
+      authenticationAlternatives = listOf(),
+      authenticationDocumentURI = null,
+      catalogURI = URI.create("https://lfavietnamcollection.cantookstation.com/catalog/featuredresources.atom"),
+      cardCreatorURI = null,
+      displayName = "Vietnam",
+      eula = null,
+      id = URI.create("urn:provider:com.cantookstation.lfavietnamcollection"),
+      idNumeric = -1,
+      isProduction = true,
+      license = null,
+      loansURI = null,
+      logo = URI.create("simplified-asset:logos/vietnam.png"),
+      mainColor = "#ec1c24",
+      patronSettingsURI = null,
+      privacyPolicy = null,
+      subtitle = "",
+      supportEmail = null,
+      supportsReservations = false,
+      updated = DateTime.now()
+    )
+}
